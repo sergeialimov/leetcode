3c1d847fdaf870346f843900df62b0735a8da85b
sergeialimov <8912652@gmail.com>
2021-04-01 13:40:08 +0300

    12-1282 add findIndexes

--- algorithms/medium/12-1282.ts
+++ algorithms/medium/12-1282.ts
@@ -1,18 +1,18 @@
 // Function finds indexes of num
-  // The number of indexes cannot be more than num
-  function findIndexes(arr: number[], num: number, startPoint: number): number[] {
-    console.log('-- -- -- startPoint', startPoint);
-    
-    const res: number[] = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (num === arr[i]) {
-        res.push(i);
-      }
-      if (res.length >= num) {
-        return res;
-      }
+// The number of indexes cannot be more than num
+function findIndexes(arr: number[], num: number, startPoint: number): number[] {
+  console.log('-- -- -- startPoint', startPoint);
+
+  const res: number[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (num === arr[i]) {
+      res.push(i);
     }
-    return res;
+    if (res.length >= num) {
+      return res;
+    }
+  }
+  return res;
   }
 
 
@@ -21,17 +21,22 @@ function groupThePeople(groupSizes: number[]): number[][] {
   const res: number[][] = [];
   let startPoint: number = 0;
 
+  const doneNumbers: number[] = [];
+
   for (let i = 0; i < groupSizes.length; i++) {
-    const element = groupSizes[i]; // 3
-    const tmp = findIndexes(groupSizes, element, startPoint);
-    res.push(tmp);
-    if (tmp.length >= element) {
-      startPoint = tmp.length;
-      // startPoint = tmp[tmp.length];
+    const element = groupSizes[i];
+    let tmp: number[] = [];
+
+    if (!doneNumbers.includes(element)) {
+      tmp = findIndexes(groupSizes, element, startPoint);
+      res.push(tmp);
+
+      doneNumbers.push(element);
+      console.log('-- -- -- tmp', tmp);
     }
-    console.log('-- -- -- tmp', tmp);
+
   }
-  
+
   console.log('\n\n-- -- -- res\n', res);
   return res;
 };
