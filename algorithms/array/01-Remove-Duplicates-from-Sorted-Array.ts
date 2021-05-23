f92f4528436db850c4cc16b75d0c0afddfc076bb
sergeialimov <8912652@gmail.com>
2021-05-23 22:45:46 +0500

    Array-01 Fix cases when more than 2 duplicates

--- algorithms/array/01-Remove-Duplicates-from-Sorted-Array.ts
+++ algorithms/array/01-Remove-Duplicates-from-Sorted-Array.ts
@@ -2,13 +2,14 @@ function removeDuplicates(nums: number[]): number {
   for(let i: number = 0; i < nums.length; i++) {
     if (nums[i + 1] === nums[i]) {
         nums.splice(i, 1);
+        i--;
     }
   }
-  console.log('-- -- -- nums', nums);
+  // console.log('-- -- -- nums', nums);
   
   return nums.length;
-};
+};bp 
 
-const res = removeDuplicates([0,0,1,1,1,2,2,3,3,4]);
+const res = removeDuplicates([1,1,2]);
+// const res = removeDuplicates([0,0,1,1,1,2,2,3,3,4]);
 console.log('-- -- -- res', res);
-
