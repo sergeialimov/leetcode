ad8a399cb1da08f702fdd12339c9d7912d65d572
sergeialimov <8912652@gmail.com>
2021-08-11 11:29:37 +0500

    add faster solution

---
+++ algorithms/array/02-Best-Time-to-Buy-and-Sell-Stock-2-map.ts
@@ -0,0 +1,24 @@
+export {}
+
+function maxProfit(prices: number[]): number {
+  let profit = 0;
+  let diff;
+
+  if (prices.length === 1) {
+    return 0;
+  }
+
+  prices.map((item, i) => {
+    const nextItem = prices[i + 1];
+    if (nextItem && nextItem > item) {
+      diff = nextItem - item;
+      profit += diff;
+    }
+  });
+
+  return profit;
+};
+
+
+const res = maxProfit([ 7, 1, 5, 3, 6, 4 ]);
+console.log('-- -- -- res', res);
