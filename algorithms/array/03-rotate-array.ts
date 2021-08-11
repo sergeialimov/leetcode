fef1ed501bf35fbcabf2cbef2d0ade3b72129342
sergeialimov <8912652@gmail.com>
2021-08-11 12:14:30 +0500

    add template for task #3

---
+++ algorithms/array/03-rotate-array.ts
@@ -0,0 +1,9 @@
+
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+ function rotate(nums: number[], k: number): void {
+  
+};
+
+console.log('-- ', rotate([1, 2, 3, 4, 5, 6, 7], 3));
