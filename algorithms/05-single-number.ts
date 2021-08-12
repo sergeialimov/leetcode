36ce0950d0e2d7a77c623412d5a00643151c22d0
sergeialimov <8912652@gmail.com>
2021-08-12 23:28:21 +0500

    add first solution of #5

--- algorithms/05-single-number.ts
+++
@@ -1,6 +0,0 @@
-function singleNumber(nums: number[]): number {
-  
-  return 0;
-};
-
-console.log('singleNumber():', singleNumber([ 2, 2, 1 ]));
---
+++ algorithms/array/05-single-number.ts
@@ -0,0 +1,11 @@
+function singleNumber(nums: number[]): number {
+  const arr = nums.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i+=2) {
+    const el = arr[i];
+    if (el !== arr[ i+1 ]) {
+      return el;
+    }
+  }
+};
+
+console.log('singleNumber():', singleNumber([ 4, 1, 2, 1, 2 ]));
