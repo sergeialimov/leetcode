ee2634722be45fef4f82fb3acfd317827702b97e
sergeialimov <8912652@gmail.com>
2021-08-12 11:09:20 +0500

    add solution fo r#4

--- algorithms/array/04-contains-duplicate.ts
+++ algorithms/array/04-contains-duplicate.ts
@@ -1,3 +1,19 @@
-function containsDuplicate(nums: number[]): boolean {
+export {}
 
+function containsDuplicate(nums: number[]): boolean {
+  if (nums.length === 0 || nums.length === 1) {
+    return false;
+  }
+  const arr = [];
+  let res = false;
+  nums.map((x) => {
+    if (arr.includes(x)) {
+      res = true;
+    }
+    arr.push(x);
+  });
+  return res;
 };
+
+const res = containsDuplicate([ 1, 2, 3, 1 ]);
+console.log('res:', res)
