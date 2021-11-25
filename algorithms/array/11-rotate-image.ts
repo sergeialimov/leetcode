3f65cb8c899e4696d212cc832bcf89ca23f6215e
sergeialimov <8912652@gmail.com>
2021-11-25 15:32:27 +0500

    clean up

---
+++ algorithms/array/11-rotate-image.ts
@@ -0,0 +1,31 @@
+import { strict as assert } from 'assert';
+
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+
+ // Outter loop. Iterate through matrix from the last element to the first
+ // Innern loop. Take the first element
+ // push it to the first 
+function rotate(matrix: number[][]): void {
+  for (let i = matrix.length-1; i >= 0; i--) {
+    const el = matrix[i];
+    let countOut = 0;
+    let countIn = 0;
+    for (let m = 0; m <= el.length-1; m++) {
+      const subEl = matrix[i][m];
+      matrixNew[countOut][countIn] = subEl;
+      countOut++;
+    }
+    countOut = 0;
+    countIn++;
+  };
+}
+
+const matrix = [[1,2,3],[4,5,6],[7,8,9]];
+console.log('-- before', matrix);
+rotate(matrix)
+console.log('-- after', matrix);
+// const expected = [[3,1],[4,2]]
+// assert.deepEqual(matrix, matrix);
+
