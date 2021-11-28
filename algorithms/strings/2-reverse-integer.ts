e408c3d73e7804e9af11f2420ca8a3c9f0d33576
sergeialimov <8912652@gmail.com>
2021-11-28 19:18:14 +0500

    task-2 add support of negative

--- algorithms/strings/2-reverse-integer.ts
+++ algorithms/strings/2-reverse-integer.ts
@@ -2,10 +2,15 @@ import { strict as assert } from 'assert';
 function reverse(x: number): number {
   const s = x.toString();
   const sArr = s.split('');
+  const isNegative = sArr[0] === '-';
+  if(isNegative) {
+    sArr.shift();
+  }
   const length =  Math.floor(sArr.length / 2);
 
   for (let i = 0; i < length; i++) {
     const first = sArr[i];
+
     const last = sArr[sArr.length - 1 - i];
 
     // assign first to last
@@ -15,11 +20,16 @@ function reverse(x: number): number {
     sArr[0 + i] = last
   }
   
-  return Number(sArr.join(''));
+  const num = Number(sArr.join(''));
+  const res = isNegative ? num * -1 : num;
+  if (res > 2147483648 - 1 || res < -2147483648) {
+    return 0;
+  }
+  return res;
 };
 
-const input = 123;
-const expected = 321;
+const input = -123;
+const expected = -321;
 const res = reverse(input);
 
 console.log('-- -- -- input', input);
