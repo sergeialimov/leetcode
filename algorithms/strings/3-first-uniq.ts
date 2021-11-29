035b90956c3d8b93e0a68f14bd7ab726dfa63c82
sergeialimov <8912652@gmail.com>
2021-11-29 12:30:26 +0500

    prepare for string-3

---
+++ algorithms/strings/3-first-uniq.ts
@@ -0,0 +1,13 @@
+import { strict as assert } from 'assert';
+
+function firstUniqChar(s: string): number {
+  const splitted = s.split('');
+  return 0;
+};
+
+
+const input = 'leetcode';
+const expected = 0;
+
+const res = firstUniqChar(input);
+assert(expected === res);
