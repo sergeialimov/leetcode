e6017eb519dccaecbf37134768f801861e8b6163
Sergei Alimov <dev.sergeialimov@gmail.com>
2022-11-24 17:07:34 +0300

    add task atm with limited cash

---
+++ atm/limited-cash/getBanknotes-original.js
@@ -0,0 +1,64 @@
+const banknotes = [
+  {
+    1: 10
+  },
+  {
+    2: 2
+  },
+  {
+    5: 5
+  },
+  {
+    10: 2
+  },
+  {
+    20: 1
+  },
+  {
+    50: 5
+  },
+  {
+    100: 10
+  },
+  {
+    200: 1
+  }
+];
+// getBanknotes(497) => {
+  // 200: 1, 100: 2, 50: 1, 20: 1, 10: 2, 5: 1, 2: 1 }
+
+  const getBanknotes = (sum) => {
+
+    // const res = {};
+    // let num = = sum; // sum to return
+
+
+    // dowhile (num !== sum) {
+      // const item = getNextItem();
+      // num -= item;
+      // if () {}
+      // res[item] += 1;
+    // }
+
+
+    // map over banknotes and return true if available
+    // isAvailable(value): boolean
+
+
+    // returns the next item from arr
+    // getNextItem(): number {}
+    // function subtract() {
+    // 
+    // }
+    // get item – 200
+
+    // if (isAvailable(item)) {
+    //   num - item
+    //   res[item] += 1; 
+    // }
+    // go to 
+
+
+
+    // return res;
+  }
