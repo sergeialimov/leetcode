dea3fe306da32c4c1e8175f5037e835d9cd21519
Sergei Alimov <dev.sergeialimov@gmail.com>
2022-12-20 09:48:14 +0300

    clean up and add comments

--- atm/limited-cash/atm-limited-cash.js
+++ atm/limited-cash/atm-limited-cash.js
@@ -1,29 +1,53 @@
-const handleAtmRequest = (sum, availableCash) => {
-  if (sum <= 0) {
+/**
+ * Version 1
+ * https://javascript.plainenglish.io/javascript-algorithms-atm-cash-machine-e6ba293c711b
+ */
+
+const handleAtmRequest = (requiredSum, availableCash) => {
+  if (requiredSum <= 0) {
     console.log('Please input an amount greater than zero');
     return;
   }
 
-  if (sum % 5 !== 0) {
+  if (requiredSum % 5 !== 0) {
     console.log('Please input the amount in multiples of 5');
     return;
   }
 
-  const denominations = [100, 50, 20, 10, 5];
+  const denominations = (Object.keys(availableCash))
+    .map((x) => +x)
+    .sort((a, b) => b - a);
+
+  // check for available sum
+  let availableSum = 0;
+  denominations.forEach((item) => availableSum += item * availableCash[item]);
+  if (availableSum < requiredSum) {
+    console.log('There\'s not enough money in the ATM');
+    return null;
+  }
+
+  // getting money
   const res = [];
 
   denominations.forEach((denomination) => {
-    while (sum >= denomination && availableCash[denomination] > 0) {
+    while (requiredSum >= denomination && availableCash[denomination] > 0) {
       res.push(denomination);
       availableCash[denomination]--;
-      sum -= denomination;
+      requiredSum -= denomination;
     }
   });
+
   return res;
 }
 
-// handleAtmRequest(-1);
-// handleAtmRequest(3);
-const res = handleAtmRequest(375, { 100: 5, 50: 3, 20: 20, 10: 5, 5: 1 });
+// const res = handleAtmRequest(375, { 100: 5, 50: 3, 20: 20, 10: 5, 5: 1 });
 // expected res [100,100,100,50,20,5]
-console.log(res);
+// console.log(res);
+
+// const res2 = handleAtmRequest(800, {100: 5, 50: 3});
+// There's not enough money in the ATM
+// console.log(res2)
+
+const res3 = handleAtmRequest(240, {100: 5, 80: 3, 20: 1});
+//
+console.log(res3)
