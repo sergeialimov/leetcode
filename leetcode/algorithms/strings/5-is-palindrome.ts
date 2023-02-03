423ab57206d6d05af53d181e92777cba430e8f78
Sergei Alimov <dev.sergeialimov@gmail.com>
2023-02-03 18:56:53 +0300

    add 5-is-palindrome

---
+++ leetcode/algorithms/strings/5-is-palindrome.ts
@@ -0,0 +1,8 @@
+function isPalindrome (s: string): boolean {
+  const str = s.toLowerCase().replace(/\W|_/g, '');
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+const res = isPalindrome("ab_a");
+console.log(res);
