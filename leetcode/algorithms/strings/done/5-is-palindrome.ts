1d9d6ed751a6d0c4d859125f202cfdc4772bccea
Sergei Alimov <dev.sergeialimov@gmail.com>
2023-02-05 16:54:09 +0300

    fix formatting

--- leetcode/algorithms/strings/done/5-is-palindrome.ts
+++ leetcode/algorithms/strings/done/5-is-palindrome.ts
@@ -1,8 +1,9 @@
 function isPalindrome (s: string): boolean {
   const str = s.toLowerCase().replace(/\W|_/g, '');
-  const reversed = str.split('').reverse().join('');
+  const reversed = str.split('').reverse()
+    .join('');
   return str === reversed;
 }
 
-const res = isPalindrome("ab_a");
+const res = isPalindrome('ab_a');
 console.log(res);
