f3968031d2fc168e7f1352068bafdb336936611e
Sergei <dev.sergeialimov@gmail.com>
2023-11-01 18:39:34 +0400

    lint commannd

--- leetcode/algorithms/array/done/01-Remove-Duplicates-from-Sorted-Array.ts
+++ leetcode/algorithms/array/done/01-Remove-Duplicates-from-Sorted-Array.ts
@@ -1,4 +1,4 @@
-function removeDuplicates (nums: number[]): number {
+export function removeDuplicates (nums: number[]): number {
   for(let i: number = 0; i < nums.length; i++) {
     if (nums[i + 1] === nums[i]) {
       nums.splice(i, 1);
