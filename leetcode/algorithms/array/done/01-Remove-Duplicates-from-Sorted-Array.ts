17404b6d4f23cbd71953a844ec358a3a2f46156a
Sergei <dev.sergeialimov@gmail.com>
2023-11-01 15:01:24 +0400

    fix formatting

--- leetcode/algorithms/array/done/01-Remove-Duplicates-from-Sorted-Array.ts
+++ leetcode/algorithms/array/done/01-Remove-Duplicates-from-Sorted-Array.ts
@@ -1,15 +1,15 @@
-function removeDuplicates(nums: number[]): number {
+function removeDuplicates (nums: number[]): number {
   for(let i: number = 0; i < nums.length; i++) {
     if (nums[i + 1] === nums[i]) {
-        nums.splice(i, 1);
-        i--;
+      nums.splice(i, 1);
+      i--;
     }
   }
   // console.log('-- -- -- nums', nums);
-  
+
   return nums.length;
 };
 
-const res = removeDuplicates([1,1,2]);
+const res = removeDuplicates([ 1,1,2 ]);
 // const res = removeDuplicates([0,0,1,1,1,2,2,3,3,4]);
 console.log('-- -- -- res', res);
