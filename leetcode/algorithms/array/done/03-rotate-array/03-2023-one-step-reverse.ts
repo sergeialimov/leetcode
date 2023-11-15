c97502b711cf046b42941392ef223602a59cf301
Sergei <dev.sergeialimov@gmail.com>
2023-11-15 16:19:02 +0300

    add 03-rotate-array/03-2023-one-step-reverse

---
+++ leetcode/algorithms/array/done/03-rotate-array/03-2023-one-step-reverse.ts
@@ -0,0 +1,33 @@
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable newline-per-chained-call */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+function rotate (nums: number[], k: number): void {
+  k %= nums.length;
+  function rotateArr (arr: number[], startInd: number, endInd: number): void {
+    while (startInd < endInd) {
+      const temp = nums[startInd];
+      nums[startInd] = nums[endInd];
+      nums[endInd] = temp;
+      startInd++;
+      endInd--;
+    }
+  }
+
+  rotateArr(nums, 0, nums.length -1);
+  rotateArr(nums, 0, k - 1);
+  rotateArr(nums, k, nums.length -1);
+};
+
+// const nums = [ 1,2,3,4,5 ];
+const nums = [ 1,2,3,4,5,6,7 ];
+// const expected = [ 4,5,1,2,3 ]
+rotate(nums, 20);
+
+console.log('-- nums', JSON.stringify(nums).split(',').join(', '));
+
+
+const nums2 = [ 1, 2, 3, 4, 5 ];
+const numsReversed = [ 5, 4, 3, 2, 1 ];
+
+
