82a75187d8397261d113f5d5b24df83b24526685
Sergei <dev.sergeialimov@gmail.com>
2023-12-01 08:33:52 +0300

    intersection base

---
+++ leetcode/algorithms/array/done/06-intersections/06-2023-intersection.ts
@@ -0,0 +1,32 @@
+import assert from 'assert';
+
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+ */
+
+
+/**
+ * Constraints:
+    1 <= nums1.length, nums2.length <= 1000
+    0 <= nums1[i], nums2[i] <= 1000
+ */
+
+/**
+ * Follow up:
+    - What if the given array is already sorted? How would you optimize your algorithm?
+    - What if nums1's size is small compared to nums2's size? Which algorithm is better?
+    - What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+ */
+function intersect (nums1: number[], nums2: number[]): number[] {
+
+
+
+
+  return [ 2,2 ];
+};
+
+assert.deepStrictEqual(intersect([ 1,2,2,1 ], [ 2, 2 ]), [ 2, 2 ], 'Arrays are not equal');
+// assert.deepStrictEqual(intersect([ 4,9,5 ], [ 9,4,9,8,4 ]), expectedArray, 'Arrays are not equal');
