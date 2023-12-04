7efafb4a143d1adb40edd2e6a58b3dd61639f2a7
Sergei <dev.sergeialimov@gmail.com>
2023-12-04 18:19:47 +0300

    move zeroes

--- leetcode/algorithms/array/done/08-move-zeroes/08-move-zeroes-2023-bitwise.ts
+++ leetcode/algorithms/array/done/08-move-zeroes/08-move-zeroes-2023-bitwise.ts
@@ -1,18 +1,20 @@
 import assert from 'assert';
 
-function moveZeroes (nums: number[]): number[] {
-  let nonZeroIndex = 0
+function moveZeroes (nums) {
+  let nonZeroIndex = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
       if (i !== nonZeroIndex) {
-        nums[nonZeroIndex] = nums[i];
-        nums[i] = 0;
+        // Swap using bitwise XOR
+        nums[i] ^= nums[nonZeroIndex];
+        nums[nonZeroIndex] ^= nums[i];
+        nums[i] ^= nums[nonZeroIndex];
       }
       nonZeroIndex++;
     }
   }
   return nums;
-};
+}
 
 assert.deepStrictEqual(moveZeroes([ 0, 0, 1,0,3,12 ]), [ 1,3,12,0,0, 0 ], 'Arrays are not equal');
 assert.deepStrictEqual(moveZeroes([ 1, 0 ]), [ 1, 0 ], 'Arrays are not equal');
