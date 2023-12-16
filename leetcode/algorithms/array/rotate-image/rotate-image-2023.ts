d33b8272282569447f609b950c9a717e0a2ec7ab
Sergei <dev.sergeialimov@gmail.com>
2023-12-16 13:48:49 +0300

    strings

--- leetcode/algorithms/array/rotate-image/rotate-image-2023.ts
+++ leetcode/algorithms/array/rotate-image/rotate-image-2023.ts
@@ -1,16 +1,21 @@
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable no-trailing-spaces */
-// import assert from 'assert';
+import assert from 'assert';
 /**
  Do not return anything, modify matrix in-place instead.
  */
 function rotate (matrix: number[][]): void {
-  
+  console.log('-- matrix');
 };
 
 
+// assert.deepStrictEqual(
+//   rotate([[ 1,2,3 ],[ 4,5,6 ],[ 7,8,9 ]]),
+//   [[ 7,4,1 ],[ 8,5,2 ],[ 9,6,3 ]],
+//   'Test 1 Failed',
+// );
 assert.deepStrictEqual(
-  rotate([[ 1,2,3 ],[ 4,5,6 ],[ 7,8,9 ]]),
-  [[ 7,4,1 ],[ 8,5,2 ],[ 9,6,3 ]],
-  'Test 1 Failed',
+  rotate([[ 1,2 ],[ 3,4 ]]),
+  [[ 4, 1 ],[ 3, 2 ]],
+  'Test 2 Failed',
 );
---
+++ leetcode/algorithms/strings/done/7-Implement-strStr().ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+import assert from 'assert';
+
+function strStr (haystack: string, needle: string): number {
+  
+  return 0;
+};
+
+assert.strictEqual(strStr('sadbutsad', 'sad'), 0, 'Test 1 Failed');
